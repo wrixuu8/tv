e04553af6dc96cd6ed803423bbd0cc3608382556
wrixuu8 <115732996+wrixuu8@users.noreply.github.com>
2023-09-15 15:48:33 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,15 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hdVideo = document.getElementById('hdVideo');
-    const sdVideo = document.getElementById('sdVideo');
-    const lowVideo = document.getElementById('lowVideo');
+// في ملف links.js
 
-    // قم بتعيين روابط الفيديو هنا
-    hdVideo.src = 'https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8';
-    sdVideo.src = 'https://cdn.smartstream.video/smartstream-us/dy365/dy365/playlist.m3u8';
-    lowVideo.src = 'رابط الفيديو منخفض الجودة';
-
-    // إعادة تشغيل الفيديو عند تغيير الرابط
-    hdVideo.load();
-    sdVideo.load();
-    lowVideo.load();
-});
+// روابط الفيديو
+const low = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
+const sd = "رابط الفيديو بجودة قياسية";
+const hd = "رابط الفيديو بجودة عالية";
