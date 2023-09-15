3550c650b12fa668918773c3d323ae6b6eb4908b
wrixuu8 <115732996+wrixuu8@users.noreply.github.com>
2023-09-15 10:55:32 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,8 +1,3 @@
 let hd = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
 let sd = ""; // قم بتعيين الرابط المناسب للجودة المنخفضة
 let low = ""; // قم بتعيين الرابط المناسب للجودة المنخفضة
-
-// تحديد المشغلات باستخدام المعرفات وتعيين عناوين URL
-document.getElementById("hdVideo").setAttribute("src", hd);
-document.getElementById("sdVideo").setAttribute("src", sd);
-document.getElementById("lowVideo").setAttribute("src", low);
