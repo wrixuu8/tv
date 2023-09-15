7dcd55fa6cda60e3f019e2aef7babd4727efdd10
wrixuu8 <115732996+wrixuu8@users.noreply.github.com>
2023-09-15 15:06:12 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,8 +1,15 @@
-let hd = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
-    let sd = "URL الجودة المنخفضة هنا"; // قم بتعيين الرابط المناسب للجودة المنخفضة
-    let low = "URL الجودة المنخفضة هنا"; // قم بتعيين الرابط المناسب للجودة المنخفضة
+document.addEventListener('DOMContentLoaded', function () {
+    const hdVideo = document.getElementById('hdVideo');
+    const sdVideo = document.getElementById('sdVideo');
+    const lowVideo = document.getElementById('lowVideo');
 
-    let vids = document.getElementsByClassName("vid");
-    vids[0].firstElementChild.setAttribute("src", low);
-    vids[1].firstElementChild.setAttribute("src", sd);
-    vids[2].firstElementChild.setAttribute("src", hd);
+    // قم بتعيين روابط الفيديو هنا
+    hdVideo.src = 'https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8';
+    sdVideo.src = 'رابط الفيديو جودة قياسية';
+    lowVideo.src = 'رابط الفيديو منخفض الجودة';
+
+    // إعادة تشغيل الفيديو عند تغيير الرابط
+    hdVideo.load();
+    sdVideo.load();
+    lowVideo.load();
+});
