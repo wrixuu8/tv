20ba9a14bd56803246b797b20df5553d2fd6f4ec
wrixuu8 <115732996+wrixuu8@users.noreply.github.com>
2023-09-15 16:32:13 +0100

    Add files via upload

--- links.js
+++ links.js
@@ -3,4 +3,4 @@
 // روابط الفيديو
 const low = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
 const sd = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
-const hd = "رابط الفيديو بجودة عالية";
+const hd = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
