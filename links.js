0b1c06ecda391b97ae2021e2ef108ad0e6e7006a
wrixuu8 <115732996+wrixuu8@users.noreply.github.com>
2024-06-24 13:19:24 +0100

    Delete links.js

--- links.js
+++
@@ -1,6 +0,0 @@
-// في ملف links.js
-
-// روابط الفيديو
-const low = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
-const sd = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
-const hd = "https://streamtv2.elitecomunicacion.cloud:3956/live/arabitvlive.m3u8";
